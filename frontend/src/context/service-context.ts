9899e3de5f867340444eed1a89fda3e8f6f69d48
Gallay Lajos <gallay.lajos@gmail.com>
2019-06-25 21:54:33 +0200

    feat(frontend):app service for localhost only

--- frontend/src/context/service-context.ts
+++ frontend/src/context/service-context.ts
@@ -1,3 +1,7 @@
 import { createContext } from "react";
 
-export const ServiceContext = createContext(process.env.APP_SERVICE_URL);
+export const ServiceContext = createContext(
+  window.location.host === "localhost"
+    ? process.env.APP_SERVICE_URL
+    : `${window.location.protocol}//${window.location.host}/`
+);
